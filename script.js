d0364c8caac6a59cae8e3121f490672625095c06
Anna Panferova <anyapanf@gmail.com>
2023-05-21 17:02:26 +0600

    mobile version is almost ready

--- script.js
+++ script.js
@@ -2,11 +2,19 @@ $(document).ready(function () {
     $(".owl-carousel").owlCarousel({
         dots: false,
         margin: 50,
-        items: 5,
+        // items: 5,
         slideTransition: `linear`,
         autoplay: 1000,
         smartSpeed: 1000,
         autoplayTimeout: 1000,
-        loop: true
+        loop: true,
+        responsive: {
+            0: {
+                items: 2,
+            },
+            768: {
+                items: 5
+            }
+        }
     });
 });
